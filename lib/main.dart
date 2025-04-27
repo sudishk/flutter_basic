bb727fb79bacc4169f2bcaa3ab1a5f990b6ae340
Sudish kumar <sudishkumar.edugaon@gmail.com>
2025-04-27 15:42:35 +0530

    Text set get, Showing toast and switch

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:fluttertoast/fluttertoast.dart';
 void main(){
   runApp(MyApp());
 }
@@ -13,106 +13,73 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: SafeArea(
-          child: ListView(
-          padding: EdgeInsets.only(top: 30),
-            children: [
+      home: HomeScreen(),
+    );
+  }
+}
 
-              Container(
-                margin: EdgeInsets.all(20),
-                height: 250,
-                child:Row(
-                  spacing: 100,
-                  children: [
-                    Text("Sudish"),
-                    Text("Sudish"),
-                  ],
-                ) ,
-                decoration: BoxDecoration(color: Colors.black12, borderRadius: BorderRadius.circular(30)),
-              ),
-              Container(
-                margin: EdgeInsets.all(20),
-                height: 250,
-                child:Row(
-                  spacing: 100,
-                  children: [
-                    Text("Sudish"),
-                    Text("Sudish"),
-                  ],
-                ) ,
-                decoration: BoxDecoration(color: Colors.black12, borderRadius: BorderRadius.circular(30)),
-              ),
-              Container(
-                margin: EdgeInsets.all(20),
-                height: 250,
-                child:Row(
-                  spacing: 100,
-                  children: [
-                    Text("Sudish"),
-                    Text("Sudish"),
-                  ],
-                ) ,
-                decoration: BoxDecoration(color: Colors.black12, borderRadius: BorderRadius.circular(30)),
-              ),
-              Container(
-                margin: EdgeInsets.all(20),
-                height: 250,
-                child:Row(
-                  spacing: 100,
-                  children: [
-                    Text("Sudish"),
-                    Text("Sudish"),
-                  ],
-                ) ,
-                decoration: BoxDecoration(color: Colors.black12, borderRadius: BorderRadius.circular(30)),
-              ),
-              Container(
-                margin: EdgeInsets.all(20),
-                height: 250,
-                child:Row(
-                  spacing: 100,
-                  children: [
-                    Text("Sudish"),
-                    Text("Sudish"),
-                  ],
-                ) ,
-                decoration: BoxDecoration(color: Colors.black12, borderRadius: BorderRadius.circular(30)),
-              ),
-              SizedBox(child: Icon(Icons.home),height: 100, width: 100,),
-              Wrap(
-                spacing: 100,
 
-                children: [
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  var isSwitchOn = false;
+  TextEditingController nameController = TextEditingController();
+  String enteredName = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Switch(value: isSwitchOn, onChanged: (value) {
+              isSwitchOn = value;
+              setState(() {
+
+              });
+            },),
+            SizedBox(child: Icon(Icons.home),height: 100, width: 100,),
+            Wrap(
+              spacing: 100,
+
+              children: [
+
                 Text("Sudish"),
                 Text("Sudish"),
-                  Text("Sudish"),
-                  Text("Sudish"),
-                  Text("Sudish"),
-                  Text("Sudish"),
-                  Text("Sudish"),
-                  Text("Sudish"),
+                Text("Sudish"),
+                Text("Sudish"),
+                Text("Sudish"),
+                Text("Sudish"),
+                Text("Sudish"),
 
-                  Text("Sudish"),
-                  Text("Sudish"),Text("Sudish"),
-                  Text("Sudish"),
+                Text("Sudish"),
+                Text("Sudish"),Text("Sudish"),
+                Text("Sudish"),
 
 
 
               ],),
-              Text("Sudish"),
-              Text("Sudish"),
-              Text("Sudish"),
-              Text("Sudish"),
-              Icon(Icons.person),
-              TextField(decoration: InputDecoration(hintText: "Enter name"),),
-              // Image.network("src"),
-              ElevatedButton(onPressed: () {
+            Text("Sudish"),
+            Text("Sudish"),
+            Text("Sudish"),
+            Text(enteredName, style: TextStyle(fontSize: 30),),
+            Icon(Icons.person),
+            TextField(controller: nameController, decoration: InputDecoration(hintText: "Enter name"),),
+            // Image.network("src"),
+            ElevatedButton(onPressed: () {
+              var name = nameController.text.toString();
+              enteredName = name;
+              setState(() {
 
-              }, child: Text("Click me"))
+              });
+              Fluttertoast.showToast(msg: "You have entered $name");
+            }, child: Text("Click me"))
 
-            ],
-          ),
+          ],
         ),
       ),
     );
