14972dc078015762598ef6dcf6228d83ffa4f52d
Sudish kumar <sudishkumar.edugaon@gmail.com>
2025-05-19 12:18:43 +0530

    Image Picker  1. Gallery  2. Camera

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,81 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_basic/home_screen.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main(){
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
-  const MyApp({super.key, });
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
   }
-
-
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomePageState extends State<HomePage> {
-  @override
-  void initState() {
-    super.initState();
+class _HomeScreenState extends State<HomeScreen> {
+
+  XFile? image;
+  void checkPermission(ImageSource source)async{
+    final ImagePicker picker = ImagePicker();
+
+    var cameraPermission = await Permission.camera.status.isGranted;
+    var galleryPermission = await Permission.photos.status.isGranted;
+    if(source == ImageSource.camera && !cameraPermission){
+      await Permission.camera.request();
+    }else if(source == ImageSource.gallery && !galleryPermission) {
+      await Permission.photos.request();
+    }
+
+    if(source == ImageSource.gallery && galleryPermission){
+      image =await picker.pickImage(source: ImageSource.gallery);
+    }else {
+      image =await picker.pickImage(source: ImageSource.camera);
+
+    }
+    setState(() {
+
+    });
+
   }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ElevatedButton(onPressed: () {
-        setState(() {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
 
-        });
-      }, child: Text("data")),
+            image != null? Image.file(File(image!.path)):Icon(Icons.browse_gallery),
+            Row(
+              children: [
+                ElevatedButton(onPressed: () {
+
+                  checkPermission(ImageSource.camera);
+                }, child: Text("Camera")),
+                ElevatedButton(onPressed: () {
+                  checkPermission(ImageSource.gallery);
+                }, child: Text("Gallery"))
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
-
-  @override
-  void didUpdateWidget(covariant HomePage oldWidget) {
-    // TODO: implement didUpdateWidget
-    super.didUpdateWidget(oldWidget);
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-  }
-
 }
-
-
 
