9358642bbfade601e4831559d4e16c14b401d020
Sudish kumar <sudishkumar.edugaon@gmail.com>
2025-05-19 18:30:13 +0530

    Provider  1. ChangeNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
-import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter_basic/home_page_provider.dart';
+import 'package:provider/provider.dart';
 
 void main(){
   runApp(MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
@@ -15,67 +14,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      home: HomePage() ,
     );
   }
 }
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-
-  XFile? image;
-  void checkPermission(ImageSource source)async{
-    final ImagePicker picker = ImagePicker();
-
-    var cameraPermission = await Permission.camera.status.isGranted;
-    var galleryPermission = await Permission.photos.status.isGranted;
-    if(source == ImageSource.camera && !cameraPermission){
-      await Permission.camera.request();
-    }else if(source == ImageSource.gallery && !galleryPermission) {
-      await Permission.photos.request();
-    }
-
-    if(source == ImageSource.gallery && galleryPermission){
-      image =await picker.pickImage(source: ImageSource.gallery);
-    }else {
-      image =await picker.pickImage(source: ImageSource.camera);
-
-    }
-    setState(() {
-
-    });
-
-  }
+class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Column(
-          children: [
+    return Scaffold(
+      body: ChangeNotifierProvider(
+        create: (context) => HomePageProvider() ,
+        builder: (context, child) {
+          var provider = context.watch<HomePageProvider>();
+        return  Column(
+            children: [
+              Text(provider.data),
 
-            image != null? Image.file(File(image!.path)):Icon(Icons.browse_gallery),
-            Row(
-              children: [
-                ElevatedButton(onPressed: () {
+              ElevatedButton(onPressed: () {
+                provider.changeData();
+              }, child: Text("Change Data"))
+            ],
+          );
+        },
 
-                  checkPermission(ImageSource.camera);
-                }, child: Text("Camera")),
-                ElevatedButton(onPressed: () {
-                  checkPermission(ImageSource.gallery);
-                }, child: Text("Gallery"))
-              ],
-            )
-          ],
-        ),
       ),
     );
   }
 }
-
