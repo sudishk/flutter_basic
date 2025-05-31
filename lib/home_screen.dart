d4378360df2092a4664417ad9052aca177a6cf56
Sudish kumar <sudishkumar.edugaon@gmail.com>
2025-05-31 20:07:31 +0530

    Future & Stream Builder

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,5 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_basic/product_screen.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,15 +9,53 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return  SafeArea(
       child: Scaffold(
-        body: ElevatedButton(onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) => ProductScreen(),));
-        }, child: Text("Go to Product Screen ")),
+        body:Column(
+          children: [
+            // Stream builder start
+
+            StreamBuilder(stream: fetchCounter(), builder: (context, snapshot) {
+              if(snapshot.hasError){
+                return Text(snapshot.error.toString());
+              } else if(snapshot.hasData){
+                return Text(snapshot.data.toString());
+              }else{
+                return Center(child: CircularProgressIndicator(),);
+              }
+            },),
+
+
+            // Stream builder end
+
+            // future builder start
+            FutureBuilder(future: fetchUser(), builder: (context, snapshot) {
+              if(snapshot.hasError){
+                return Container(color: Colors.red, child: Text(snapshot.error.toString()),);
+              } else if(snapshot.hasData){
+                return Container(color: Colors.grey, child: Text(snapshot.data.toString()),);
+              }
+              else{
+                return Center(child: CircularProgressIndicator(),);
+              }
+            },)
+            // future builder end
+
+          ],
+        ) ,
       ),
+
     );
   }
+
+ Future<String> fetchUser()async{
+   return await Future.delayed(Duration(seconds: 5), () => "Mohan",);
+  }
+
+  Stream<int> fetchCounter(){
+    return  Stream.periodic(Duration(seconds: 2), (computationCount) => computationCount+1,);
+  }
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_basic/home_screen.dart';
+
+import 'home_screen.dart';
 
 void main(){
   runApp(MyApp());
