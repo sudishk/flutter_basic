6013361ff058be5c106eb7c082f3cfbff648843a
Sudish kumar <sudishkumar.edugaon@gmail.com>
2025-06-03 13:56:02 +0530

    Tween Animation

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,7 +8,45 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin{
+  late AnimationController animationController;
+  late Animation<double> animation;
+  late Animation<Color?> colorAnimation;
+  late Animation<Alignment> alignAnimation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    animationController = AnimationController(vsync: this, duration: Duration(seconds: 5));
+
+    animation = Tween<double>(begin: 0.0, end: 300.0).animate(animationController)
+      ..addListener(() {
+      setState(() {
+
+      });
+    },);
+
+    // colorAnimation = ColorTween(begin: Colors.red, end: Colors.blueGrey,).animate(animationController);
+    colorAnimation = TweenSequence<Color?>([
+      TweenSequenceItem(tween: ColorTween(begin: Colors.orange ,end: Colors.green), weight: 1),
+      TweenSequenceItem(tween: ColorTween(begin: Colors.brown ,end: Colors.grey), weight: 2),
+      TweenSequenceItem(tween: ColorTween(begin: Colors.purpleAccent ,end: Colors.cyan), weight: 1),
+    ]).animate(animationController);
+
+    alignAnimation = AlignmentTween(end: Alignment.bottomCenter, begin: Alignment.bottomRight).animate(animationController);
+    animationController.forward();
+
+
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    animationController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return  SafeArea(
@@ -17,6 +55,16 @@ class _HomeScreenState extends State<HomeScreen> {
           children: [
             // Stream builder start
 
+            Container(
+              height: animation.value,
+              width: animation.value,
+              color: colorAnimation.value,
+
+            ),
+
+            Align(
+                alignment: alignAnimation.value,
+                child: Container( height: 40,width: 50, color: Colors.deepOrange,)),
             StreamBuilder(stream: fetchCounter(), builder: (context, snapshot) {
               if(snapshot.hasError){
                 return Text(snapshot.error.toString());
