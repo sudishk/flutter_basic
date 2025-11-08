3c1714e6f0c80468633720146e494aada067b076
Sudish kumar <sudishkumar.edugaon@gmail.com>
2025-11-08 10:33:22 +0530

    Riverpod counter app added

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,36 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_basic/home_screen.dart';
+import 'package:riverpod/legacy.dart';
+import 'package:riverpod/riverpod.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
   }
 }
+
+class HomeScreen extends StatelessWidget {
+  final counterProvider = StateProvider<int>((ref) => 0);
+  HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final count = Provider((ref) => counterProvider,)
+        count.watch(counterProvider);
+
+    return const Placeholder();
+  }
+}
+
+
+
 
 
