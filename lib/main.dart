203155111397b0ea1515673d106f92c0b9a1423d
Sudish kumar <sudishkumar.edugaon@gmail.com>
2025-11-20 09:44:37 +0530

    Nested List converted to simple list

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,41 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_basic/home_screen.dart';
+// void main() {
+//   var nums = [40,10,4,2,90,2,4,4];
+//   var map = {};
+//
+//   for (var num in nums) {
+//     map[num] = (map[num] ?? 0) + 1;
+//   }
+//
+//   print(map);
+// }
+
+// void main() {
+//   String s = "Ram is a boy";
+//   List<String> words = s.split(" ");
+//   String result = "";
+//
+//   for (var word in words) {
+//     String reversed = "";
+//     for (int i = word.length - 1; i >= 0; i--) {
+//       reversed += word[i];
+//     }
+//
+//     result += reversed + " ";
+//   }
+//
+//   // remove last extra space (optional)
+//   print(result.trim());
+// }
 
 void main(){
-  runApp(MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-    );
+  var a = [[1,2],[3,4],[5]];
+  var b = [];
+  for (var element in a) {
+    for (var element1 in element) {
+      b.add(element1);
+    }
   }
-}
 
+  print(b);
+}
 
