23d58ab30ececc92c4fd8aad68019913868fdb49
Sudish kumar <sudishkumar.edugaon@gmail.com>
2025-11-22 10:33:25 +0530

    App check implemented

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:razorpay_flutter/razorpay_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,55 +8,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  var amountController = TextEditingController();
-  Razorpay? razorpay;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    razorpay = Razorpay();
-    razorpay?.on(Razorpay.EVENT_PAYMENT_SUCCESS, (PaymentSuccessResponse success){
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Payment successfully ${success.paymentId}")));
-    });
-    razorpay?.on(Razorpay.EVENT_PAYMENT_ERROR, (PaymentFailureResponse error){
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Payment failed ${error.message}")));
-
-    });
-    razorpay?.on(Razorpay.EVENT_EXTERNAL_WALLET, (ExternalWalletResponse wallet){
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Payment successfully ${wallet.walletName}")));
-
-    });
-  }
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
-      body: Column(
-        children: [
-          TextField(controller: amountController, decoration: InputDecoration(hint: Text("Enter amount")),),
-          ElevatedButton(onPressed: () {
-            var amount = int.parse(amountController.text) * 100;
-
-            var options = {
-              'key': 'rzp_test_R7xQYpa54gC33c',
-              'amount': "$amount",
-              'order_id': "",
-              'name': 'Acme Corp.',
-              'description': 'Fine T-Shirt',
-              'prefill': {
-                'contact': '8888888888',
-                'email': 'test@razorpay.com'
-              }
-            };
-            razorpay?.open(options);
-          }, child: Text("Pay Now"))
-        ],
-      ),
-    ));
-  }
-  @override
-  void dispose() {
-    super.dispose();
-    razorpay?.clear();
+    return Container();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_basic/home_screen.dart';
 
-void main(){
+import 'firebase_options.dart';
+
+void main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FirebaseAppCheck.instance.activate(
+    providerAndroid: AndroidDebugProvider(),
+    providerApple: AppleAppAttestProvider(),
+    providerWeb: ReCaptchaV3Provider("6LflWxIsAAAAABPx0iDFz-fjZamaBrZ1zfVffCqM")
+  );
   runApp(MyApp());
 }
 
