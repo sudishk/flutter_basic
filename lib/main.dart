4134114eb9ab3150c2bf173aac8e7270d84fa61b
Sudish kumar <sudishkumar.edugaon@gmail.com>
2025-11-22 10:41:40 +0530

    Palindrome and Armstrong added

--- lib/main.dart
+++ lib/main.dart
@@ -27,15 +27,48 @@
 //   print(result.trim());
 // }
 
+// void main(){
+//   var a = [[1,2],[3,4],[5]];
+//   var b = [];
+//   for (var element in a) {
+//     for (var element1 in element) {
+//       b.add(element1);
+//     }
+//   }
+//
+//   print(b);
+// }
+
+// void main(){
+//   var a=202;
+//   var rev = 0;
+//   var temp = a;
+//   while(temp !=0){
+//     var rem = temp %10;
+//     rev = rev * 10 + rem;
+//     temp = temp ~/10;
+//   }
+//
+//   if(a==rev){
+//     print("Palindrome");
+//   }else{
+//     print("Not Palindrome");
+//   }
+// }
 void main(){
-  var a = [[1,2],[3,4],[5]];
-  var b = [];
-  for (var element in a) {
-    for (var element1 in element) {
-      b.add(element1);
-    }
+  var a=153;
+  var rev = 0;
+  var temp = a;
+  while(temp !=0){
+    var rem = temp %10;
+    rev = rev + (rem*rem*rem);
+    temp = temp ~/10;
   }
 
-  print(b);
+  if(a==rev){
+    print("Armstrong");
+  }else{
+    print("Not Armstrong");
+  }
 }
 
