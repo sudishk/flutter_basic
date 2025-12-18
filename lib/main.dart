fef4adea13bf61686ea96cf5c78dced8a9be2eab
Sudish kumar <sudishkumar.edugaon@gmail.com>
2025-12-18 11:47:30 +0530

    Isolates is used

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,23 @@
-import 'package:firebase_app_check/firebase_app_check.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_basic/home_screen.dart';
+import 'dart:async';
+import 'dart:isolate';
 
-import 'firebase_options.dart';
+import 'package:flutter/foundation.dart';
 
 void main()async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
 
-  FirebaseAppCheck.instance.activate(
-    providerAndroid: AndroidDebugProvider(),
-    providerApple: AppleAppAttestProvider(),
-    providerWeb: ReCaptchaV3Provider("6LflWxIsAAAAABPx0iDFz-fjZamaBrZ1zfVffCqM")
-  );
-  runApp(MyApp());
+  final upload =await compute(uploadImage, 1);
+  print(upload);
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+int uploadImIage(int a){
+  int count = 0;
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomeScreen(),
-    );
-  }
+  Timer.periodic(Duration(seconds: a), (timer) {
+    count++;
+    if(count==10){
+      timer.cancel();
+    }
+
+  },);
+  return count;
 }
