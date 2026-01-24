b0444fc9eb4504466d4aa8b809c7f649d7ebf664
Sudish kumar <sudishkumar.edugaon@gmail.com>
2026-01-24 18:49:52 +0530

    Json placeholder api called

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_basic/post_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod/legacy.dart';
-
+import 'package:http/http.dart' as http;
 import 'counter_notifier.dart';
 import 'counter_state.dart';
 
+final postsProvider = FutureProvider<List<PostModel>>((ref) async {
+  return fetchPosts();
+});
 final counterProvider = StateProvider<int>((ref) => 0);
 final counterNotifierProvider = StateNotifierProvider<CounterNotifier, CounterState>((ref) => CounterNotifier(),);
 void main() {
@@ -32,27 +38,38 @@ class HomeScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final count = ref.watch(counterNotifierProvider);
+    final postsAsync = ref.watch(postsProvider);
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Riverpod Counter'),
-      ),
-      body: Center(
-        child: Text(
-          count.count.toString(),
-          style: const TextStyle(
-            fontSize: 40,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-         ref.read(counterNotifierProvider.notifier).incrementCounter();
+      appBar: AppBar(title: const Text("JSONPlaceholder API")),
+      body: postsAsync.when(
+        loading: () => const Center(child: CircularProgressIndicator()),
+        error: (error, _) => Center(child: Text(error.toString())),
+        data: (posts) {
+          return ListView.builder(
+            itemCount: posts.length,
+            itemBuilder: (_, index) {
+              final post = posts[index];
+              return ListTile(
+                title: Text(post.title),
+                subtitle: Text(post.body),
+              );
+            },
+          );
         },
-        child: const Icon(Icons.add),
       ),
     );
+  }
+}
+Future<List<PostModel>> fetchPosts() async {
+  final response = await http.get(
+    Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+  );
+
+  if (response.statusCode == 200) {
+    final List data = jsonDecode(response.body);
+    return data.map((e) => PostModel.fromJson(e)).toList();
+  } else {
+    throw Exception('Failed to load posts');
   }
 }
